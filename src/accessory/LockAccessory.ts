12d2b0aa3462744cd8b410dd80c595160f499b68
pfgimutao <110768704+pfgimutao@users.noreply.github.com>
2023-02-01 13:10:01 +0800

    Debug 4

--- src/accessory/LockAccessory.ts
+++ src/accessory/LockAccessory.ts
@@ -1,52 +1,24 @@
-import { PlatformAccessory, Service, Characteristic } from 'homebridge';
-import { TuyaDevice } from 'tuyapi';
+import { PlatformAccessory } from 'homebridge';
+import { TuyaPlatform } from '../platform';
+import BaseAccessory from './BaseAccessory';
 
 export default class LockAccessory extends BaseAccessory {
 
-  constructor(accessory: PlatformAccessory, deviceId: string, localKey: string) {
-    this.locked = false;
-    this.accessory = accessory;
-    this.lockService = this.accessory.getService(Service.LockMechanism);
-    this.tuyaDevice = new TuyaDevice({
-      id: deviceId,
-      key: localKey,
-    });
-    this.tuyaDevice.on('connected', () => {
-      this.tuyaDevice.get({schema: true}).then((data: any) => {
-        log.debug('Tuya Device Connected', data);
-      });
-    });
-    this.tuyaDevice.connect();
-    this.lockService.getCharacteristic(Characteristic.LockCurrentState)
-      .on('get', this.getLockState.bind(this));
-    this.lockService.getCharacteristic(Characteristic.LockTargetState)
-      .on('get', this.getLockState.bind(this))
-      .on('set', this.setLockState.bind(this));
-  }
+  constructor(platform: TuyaPlatform, accessory: PlatformAccessory) {
+    super(platform, accessory);
 
-  getLockState(callback: (error: any, state: number) => void) {
-    this.tuyaDevice.get({dps: '1'}).then((state: any) => {
-      callback(null, state['1'] ? Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED);
-    });
-  }
+    const service = this.accessory.getService(this.Service.LockMechanism)
+      || this.accessory.addService(this.Service.LockMechanism);
 
-  setLockState(state: number, callback: (error: any) => void) {
-    if (state === Characteristic.LockTargetState.SECURED) {
-      this.lock();
-    } else if (state === Characteristic.LockTargetState.UNSECURED) {
-      this.unlock();
-    }
-    callback(null);
-  }
-
-  async lock() {
-    try {
-      await this.tuyaDevice.set({set: true, dps: 1});
-      this.locked = true;
-      this.lockService.updateCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED);
-      log.debug('JTMSPro Lock engaged');
-    } catch (err) {
-      log.debug('Error setting JTMSPro lock state: ', err);
+    if (this.device.getDeviceStatus('lock_motor_state')) {
+      service.getCharacteristic(this.Characteristic.LockCurrentState)
+        .onGet(() => {
+          const status = this.device.getDeviceStatus('lock_motor_state');
+          return (status?.value as boolean) ?
+            this.Characteristic.LockCurrentState.UNSECURED :
+            this.Characteristic.LockCurrentState.SECURED;
+        });
     }
   }
+
 }
