4ad28cec455dc6cfef389629ac380d2ae8c76ca2
pfgimutao <110768704+pfgimutao@users.noreply.github.com>
2023-02-01 12:39:27 +0800

    Test 1

--- src/accessory/LockAccessory.ts
+++ src/accessory/LockAccessory.ts
@@ -2,30 +2,30 @@ import { PlatformAccessory, Service, Characteristic } from 'homebridge';
 import { TuyaDevice } from 'tuyapi';
 
 export class TuyaSmartLockJTMSPro {
-    private locked: boolean;
-    private accessory: PlatformAccessory;
-    private lockService: Service;
-    private tuyaDevice: TuyaDevice;
+  private locked: boolean;
+  private accessory: PlatformAccessory;
+  private lockService: Service;
+  private tuyaDevice: TuyaDevice;
 
-    constructor(accessory: PlatformAccessory, deviceId: string, localKey: string, log: any) {
-        this.locked = false;
-        this.accessory = accessory;
-        this.lockService = this.accessory.getService(Service.LockMechanism);
-        this.tuyaDevice = new TuyaDevice({
-            id: deviceId,
-            key: localKey
+  constructor(accessory: PlatformAccessory, deviceId: string, localKey: string) {
+    this.locked = false;
+    this.accessory = accessory;
+    this.lockService = this.accessory.getService(Service.LockMechanism);
+    this.tuyaDevice = new TuyaDevice({
+      id: deviceId,
+      key: localKey
+    });
+    this.tuyaDevice.on('connected', () => {
+    this.tuyaDevice.get({schema: true}).then((data: any) => {
+      log.debug('Tuya Device Connected', data);
         });
-        this.tuyaDevice.on('connected', () => {
-            this.tuyaDevice.get({schema: true}).then((data: any) => {
-                log.debug('Tuya Device Connected', data);
-            });
-        });
-        this.tuyaDevice.connect();
-        this.lockService.getCharacteristic(Characteristic.LockCurrentState)
-            .on('get', this.getLockState.bind(this));
-        this.lockService.getCharacteristic(Characteristic.LockTargetState)
-            .on('get', this.getLockState.bind(this))
-            .on('set', this.setLockState.bind(this));
+    });
+    this.tuyaDevice.connect();
+    this.lockService.getCharacteristic(Characteristic.LockCurrentState)
+     .on('get', this.getLockState.bind(this));
+    this.lockService.getCharacteristic(Characteristic.LockTargetState)
+     .on('get', this.getLockState.bind(this))
+     .on('set', this.setLockState.bind(this));
     }
 
     getLockState(callback: (error: any, state: number) => void) {
