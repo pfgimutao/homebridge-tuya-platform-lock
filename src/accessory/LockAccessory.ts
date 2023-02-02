6a4fc3a2b08c47bdd7fb2bd8e89ec45dcbc342b2
pfgimutao <110768704+pfgimutao@users.noreply.github.com>
2023-02-02 21:00:10 +0800

    Update LockAccessory.ts

--- src/accessory/LockAccessory.ts
+++ src/accessory/LockAccessory.ts
@@ -1,43 +1,70 @@
-import { PlatformAccessory } from 'homebridge';
-import { TuyaPlatform } from '../platform';
 import BaseAccessory from './BaseAccessory';
+
+const SCHEMA_CODE = {
+  CURRENT_LOCK_STATE: ['lock_motor_state'],
+  TARGET_LOCK_STATE: ['special_control'],
+};
 
 export default class LockAccessory extends BaseAccessory {
 
-  constructor(platform: TuyaPlatform, accessory: PlatformAccessory) {
-    super(platform, accessory);
-
-    const service = this.accessory.getService(this.Service.LockMechanism)
-      || this.accessory.addService(this.Service.LockMechanism);
-
-    if (this.device.getDeviceStatus('special_control')) {
-      service.getCharacteristic(this.Characteristic.LockCurrentState)
-        .onGet(() => {
-          const status = this.device.getDeviceStatus('special_control');
-          return (status?.value as boolean) ?
-            this.Characteristic.LockCurrentState.UNSECURED :
-            this.Characteristic.LockCurrentState.SECURED;
-        });
-    }
-
-    if (this.device.getDeviceStatus('special_control')) {
-      // TODO
-      service.getCharacteristic(this.Characteristic.LockTargetState)
-        .onGet(() => {
-          const status = this.device.getDeviceStatus('special_control');
-          return (status?.value as boolean) ?
-            this.Characteristic.LockTargetState.UNSECURED :
-            this.Characteristic.LockTargetState.SECURED;
-        })
-        .onSet(value => {
-          const status = this.device.getDeviceStatus('special_control');
-          this.deviceManager.sendCommands(this.device.id, [{
-            code: status!.code,
-            value: (value === this.Characteristic.LockTargetState.UNSECURED) ? true : false, // confused value
-          }]);
-        });
-    }
-
+  requiredSchema() {
+    return [SCHEMA_CODE.TARGET_LOCK_STATE];
   }
 
+  configureServices() {
+
+    this.configureCurrentLockState();
+    this.configureTargetLockState();
+  }
+
+  mainService() {
+    return this.accessory.getService(this.Service.LockMechanism)
+      || this.accessory.addService(this.Service.LockMechanism);
+  }
+
+  configureCurrentLockState() {
+    const { LOCKED, UNLOCKED, LOCKING, UNLOCKING, STOPPED } = this.Characteristic.CurrentLockState;
+    this.mainService().getCharacteristic(this.Characteristic.CurrentLockState)
+      .onGET(() => {
+        const currentSchema = this.getSchema(...SCHEMA_CODE.CURRENT_LOCK_STATE);
+        const targetSchema = this.getSchema(...SCHEMA_COD.TARGET_LOCK_STATE);
+        if (!currentSchema || !targetSchema) {
+          return STOPPED;
+        }
+
+        const currentStatus = this.getStatus(currentSchema.code)!;
+        const targetStatus = this.getStatus(targetSchema.code)!;
+        if (currentStatus.value === false && targetStatus.value === false) {
+          return LOCKED;
+        } else if (currentStatus.value === true && targetStatus.value === false) {
+          return LOCKING;
+        } else if (currentStatus.value === false && targetStatus.value === true) {
+          return UNLOCKED;
+        } else if (currentStatus.value === true && targetStatus.value === true) {
+          return UNLOCKING;
+        }
+
+      return STOPPED;
+    });
+  }
+  
+  configureTargetLockState() {
+    const schema = this.getSchema(...SCHEMA_CODE.TARGET_LOCK_STATE);
+    if (!schema) {
+      return;
+    }
+
+    const { LOCKED, UNLOCKED } = this.Characteristic.TargetLockState;
+    this.mainService(),getCharacteristic(this.Characteristic.TargetLockState)
+      .onGet(() => {
+        const status = this.getStatus*schema.code)!;
+        return status.value as boolean ? LOCKED ; UNLOCKED;
+      })
+      .onSet(value => {
+        this.sendCommands([{
+          code: schema.code,
+          value: (value === LOCKED) ? false : true,
+        }]);
+    });
+  }
 }
